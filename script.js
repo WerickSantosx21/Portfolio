f0ab23af3e26081b8418b84a776a10283ecf9b88
Werick Santos <149383332+WerickSantosx21@users.noreply.github.com>
2025-11-03 14:02:45 -0300

    Construção de portfolío pessoal com HTML, CSS e JavaScript.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Botão de voltar ao topo
+const btnTop = document.getElementById("btn-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+});
+
+btnTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
